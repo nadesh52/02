050063b3992a47e78aefbb90bb987c5ece448fc3
nadesh <worldaroundmee@gmail.com>
2023-12-12 01:18:06 +0700

    count

---
+++ src/Count.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@mui/base";
+import { Typography } from "@mui/material";
+import React, { useState } from "react";
+
+function Count() {
+  const [count, setCount] = useState(0);
+  const [num, setNum] = useState(0);
+
+  const handleClick = (e: any) => {
+    if (e.target.name === "up") {
+      setCount(count + 1);
+    } else {
+      setCount(count - 1);
+    }
+    setNum(count);
+    console.log(num);
+  };
+  return (
+    <div>
+      <Typography variant="h1">{num}</Typography>
+      <Button name="down" onClick={(e) => handleClick(e)}>
+        -
+      </Button>
+      <Button name="up" onClick={(e) => handleClick(e)}>
+        +
+      </Button>
+    </div>
+  );
+}
+
+export default Count;
